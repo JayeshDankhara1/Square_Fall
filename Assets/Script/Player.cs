343603f0522f7bfa5406ebfbd05eed9bd33df219
JayeshDankhara1 <dankharajayesh@gmail.com>
2024-10-22 08:33:23 +0530

    PlayerAdd

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Update()
+    {
+       
+    }
+
+
+    public void PlayerMove()
+    {
+
+        
+       
+    }
+}
